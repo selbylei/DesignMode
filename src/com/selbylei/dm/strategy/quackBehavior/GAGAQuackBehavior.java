81d83cff3e2f2b3622d1f1620a9f366fa1fe97f8
selbylei <selbylei@126.com>
2017-03-12 00:48:37 +0800

    strategy mode

---
+++ src/com/selbylei/dm/strategy/quackBehavior/GAGAQuackBehavior.java
@@ -0,0 +1,12 @@
+package com.selbylei.dm.strategy.quackBehavior;
+
+/**
+ * Created by selbylei on 17/3/11.
+ */
+public class GaGaQuackBehavior implements QuackBehavior {
+
+    @Override
+    public void quack() {
+        System.out.println("gaga～～");
+    }
+}
